18e845cd38b6963ddbe699e8870763f203185fc1
Daniel Cuturilo <daniel.cuturilo@fer.hr>
2017-07-17 23:48:53 +0200

    Fixed register screen

--- Pokedex/Pokedex/RegisterViewController.swift
+++ Pokedex/Pokedex/RegisterViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let emailImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let nicknameImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let passwordImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let passwordConfirmImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        
+        emailTextField.leftViewMode = UITextFieldViewMode.always
+        nicknameTextField.leftViewMode = UITextFieldViewMode.always
+        passwordTextField.leftViewMode = UITextFieldViewMode.always
+        passwordConfirmTextField.leftViewMode = UITextFieldViewMode.always
+        
+        emailImageView.image = UIImage(named: "ic-mail")
+        nicknameImageView.image = UIImage(named: "ic-person")
+        passwordImageView.image = UIImage(named: "ic-lock")
+        passwordConfirmImageView.image = UIImage(named: "ic-lock")
+        
+        emailTextField.leftView = emailImageView
+        nicknameTextField.leftView = nicknameImageView
+        passwordTextField.leftView = passwordImageView
+        passwordConfirmTextField.leftView = passwordConfirmImageView
+        
     }
 
     override func didReceiveMemoryWarning() {
